ac8dfedbdd58afe23336bdf13c5306f70d2f52fa
Mfuon Leon <mfolee@gmail.com>
2026-02-10 06:28:20 +0300

    fix-migration issues

---
+++ drizzle/migrations/20260208_migrate_borrowed_items_v1.sql
@@ -0,0 +1,4 @@
+-- Migration: Add returned_quantity tracking to borrowed_items
+-- This allows tracking how many borrowed items have been returned
+
+ALTER TABLE borrowed_items ADD COLUMN returned_quantity INTEGER DEFAULT 0;
